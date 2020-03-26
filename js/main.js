cc7c275a2937b0534ffda8f23ced5861a2d4f174
Peter <pdnguyen714@gmail.com>
2020-03-26 09:35:10 -0700

    refactored & finished JS

--- js/main.js
+++ js/main.js
@@ -1,39 +1,32 @@
-console.log("Working");
+/* ===== Link Check ===== */
 
-/*----- constants -----*/
+console.log("JS Check");
+
+/* ===== constants ===== */
 
 const dealer = "Dealer";
 const player = "You";
 const tie = "tie";
 
-/*----- app's state variables (mutable) -----*/
+
+/* ===== app states (mutables) ===== */
 
 let playerHand = [];
 let dealerHand = [];
-let iswinner = null;
+let isWinner = null;
 let pScore = 0;
 let dScore = 0;
 
-/*----- functions -----*/
 
-function startOfGame() {
-  startGame();
-}
+/* ===== functions ===== */
 
-function startGame() {
-  initialHand();
-  render();
-  calculateScore();
-  checkForWinner();
-}
 
-startGame();
-//Fisher-Yate's shuffle algorithm
+//Fisher-Yate's Shuffle algorithm
 
 function shuffle(deck) {
   let i = 0,
-    j = 0,
-    temp = null;
+  j = 0,
+  temp = null;
   for (i = deck.length - 1; i > 0; i -= 1) {
     j = Math.floor(Math.random() * (i + 1));
     temp = deck[i];
@@ -42,7 +35,19 @@ function shuffle(deck) {
   }
 }
 
-//gives 2 cards to the dealer and player at the start
+// begins the initial state of game
+
+function startGame() {
+  hideResetBtn();
+  initialHand();
+  calculateScore(dealerHand);
+  calculateScore(playerHand);
+  render();
+}
+
+startGame();
+
+//distributes 2 cards to the dealer and player at the start
 
 function initialHand() {
   shuffle(deck);
@@ -53,92 +58,156 @@ function initialHand() {
   return playerHand, dealerHand;
 }
 
+//calculates the score
+
+function calculateScore(hand) {
+  let score = 0;
+  let aces = 0;
+
+  hand.forEach(function(card) {
+    score += card.value;
+    if (card.value === 11) {
+      aces++;
+    }
+  });
+  while (score > 21 && aces) {
+    score -= 10;
+    aces--;
+  }
+  return score;
+}
+
 //adds a card to the player's hand
 
 function hit() {
   playerHand.push(deck.pop());
-  render();
-  calculateScore();
-}
-
-function stay() {
-  while (dScore < 18) {
-    dealerHand.push(deck.pop());
-    render();
-  }
-  calculateScore();
-}
-
-function reset() {
-  startGame();
-}
-
-function calculateScore() {
-  let pScore = 0;
-  let dScore = 0;
-
-  playerHand.forEach(function(card) {
-    pScore += card.value;
-    document.getElementById("player-score").innerHTML = "Total: " + pScore;
-    return pScore;
-  });
-
-  dealerHand.forEach(function(card) {
-    dScore += card.value;
-    document.getElementById("dealer-score").innerHTML = "Total: " + dScore;
-    return dScore;
-  });
-
-  if (pScore > 21 && dScore < 21) {
-    iswinner = dealer;
+  pScore = calculateScore(playerHand);
+  if (pScore > 21) {
+    isWinner = dealer;
     checkForWinner();
   }
+  document.getElementById('dealer-score').innerHTML = "Score: " + dScore;
+  document.getElementById('player-score').innerHTML = "Score: " + pScore;
+  render();
 }
+
+//ends turn and keeps hand
+
+function stay() {
+  console.log(dealerHand);
+  console.log(playerHand);
+  dScore = calculateScore(dealerHand);
+  pScore = calculateScore(playerHand);
+
+  while (dScore < 17 && !isWinner) {
+    dealerHand.push(deck.pop());
+    dScore = calculateScore(dealerHand);
+  }
+  if (dScore > 21) {
+    isWinner = player;
+  } else if (dScore > pScore) {
+    isWinner = dealer;
+  } else if (dScore < pScore) {
+    isWinner = player;
+  } else if (dScore === pScore) {
+    isWinner = tie;
+  }
+  console.log(isWinner);
+  checkForWinner();
+  render();
+}
+
+//renders cards to browser
 
 function render() {
   document.getElementById("dealer-hand").innerHTML = "";
-  dealerHand.forEach(card => {
-    let dealerCards = `<div class="card ${card.face}"></div>`;
+  dealerHand.forEach((card, idx) => {
+    let dealerCards = `<div class="card ${
+      idx === 1 && !isWinner ? "back" : card.face
+    }"></div>`;
     document.getElementById("dealer-hand").innerHTML += dealerCards;
   });
-
   document.getElementById("player-hand").innerHTML = "";
   playerHand.forEach(card => {
     let playerCards = `<div class="card ${card.face}"></div>`;
     document.getElementById("player-hand").innerHTML += playerCards;
   });
-
-  checkForWinner();
 }
 
 // checks for winner
 
 function checkForWinner() {
-  if (iswinner !== null) {
-    if (iswinner == player) {
+  if (isWinner !== null) {
+    if (isWinner == player) {
       document.getElementById("winner").innerHTML = "Congrats! You won.";
-    } else if (iswinner == dealer) {
-      document.getElementById("winner").innerHTML = "Sorry, you lost :(";
-    } else {
-      document.getElementById("winner").innerHTML = "It's a tie!";
+    } else if (isWinner == dealer) {
+      document.getElementById("winner").innerHTML = "Sorry, you lost! But play again!";
     }
+  } else if (isWinner == tie) {
+    document.getElementById("winner").innerHTML = "It's a tie!";
   }
+  document.getElementById("player-score").innerHTML = "Score: " + pScore;
+  document.getElementById("dealer-score").innerHTML = "Score: " + dScore;
+  hide();
+  showResetBtn();
+}
+
+//hide buttons when there is a winner
+
+function hide() {
+  $('#hit').hide();
+  $('#stay').hide();
+}
+
+// hides reset button when there is no winner
+
+function hideResetBtn() {
+  $('#reset').hide()
+}
+
+// shows reset button when there is a winner
+
+function showResetBtn() {
+  $('#reset').show()
+}
+
+//show hit & stay buttons when player resets
+
+function showBtns() {
+  $('#hit').show();
+  $('#stay').show();
+}
+
+//clears scores of previous game
+
+function clearScores() {
+  document.getElementById("dealer-score").innerHTML = "Score: ";
+  document.getElementById("player-score").innerHTML = "Score: ";
 }
 
 
-/*----- event listeners -----*/
+//event listeners
 
-let hitButton = document.getElementById("hit");
-hitButton.addEventListener("click", function(e) {
+let $toggleRules = $('#rulesBtn').on('click', function(e) {
+  $('.rules').fadeToggle('slow', "linear");
+})
+
+let $hitButton = $('#hit').on('click', function(e) {
   hit();
-});
+})
 
-let stayButton = document.getElementById("stay");
-stayButton.addEventListener("click", function(e) {
+let $stayButton = $('#stay').on('click', function(e){
   stay();
-});
+})
 
 let resetButton = document.getElementById("reset");
+
 resetButton.addEventListener("click", function(e) {
-  reset();
+  document.getElementById("winner").innerHTML = " ";
+  clearScores();
+  playerHand = [];
+  dealerHand = [];
+  isWinner = null;
+  startGame();
+  showBtns();
 });
